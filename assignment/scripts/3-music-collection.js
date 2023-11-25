1c7818917c83b7c0db3cc77e373df023e88513b7
Lauren McGarvey <l.al.mcgarvey@gmail.com>
2023-11-25 13:42:52 -0500

    Created function to add an album to myCollection. Added function to display collection within console. Created a search function to find specific artists.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -1,12 +1,51 @@
 console.log('***** Music Collection *****')
 // Safe Zone -- Write code below this line
 
+let myCollection = []; // Create myCollection array
+function addToCollection (collection, title, artist, yearPublished){ // Create addToCollection function with expected parameters.
+  const recordDetails = {  // Creates new object names recordDetails
+    albumTitle: title,     // Adds albumTitle as object attribute
+    albumArtist: artist,   // Adds albumArtist as object attribute
+    releaseDate: yearPublished, // Adds releaseDate as object attribute
+  };
+  collection.push(recordDetails); //Updates the passed in array with the recordDetails object
+  return recordDetails;           //Returns object values
+};
 
+addToCollection(myCollection, "Jester's Dance", "In Flames", "1996");
+console.log(myCollection);
+addToCollection(myCollection, "White Crosses/Black Crosses", "Against Me!", "2010");
+console.log(myCollection);
+addToCollection(myCollection, "Brothers", "The Black Keys", "2010");
+console.log(myCollection);
+addToCollection(myCollection, "Panic", "Caravan Palace", "2012");
+console.log(myCollection);
+addToCollection(myCollection, "Imaginary Diseases", "Frank Zappa", "2006");
+console.log(myCollection);
+addToCollection(myCollection, "Predator's Potrait", "Soilwork", "2001");
+console.log(myCollection);
+addToCollection(myCollection, "Come Clarity", "In Flames", "2006");
+console.log(myCollection);
 
+function showCollection (collection){
+  for (let collectionIndex of collection){
+    console.log(`${collectionIndex.albumTitle} by ${collectionIndex.albumArtist}, published in ${collectionIndex.releaseDate}`)
+  };
+};
+showCollection(myCollection);
 
+function findByArtist(collection, artist){
+  let results = [];
+  for (let collectionIndex of collection){
+    if(collectionIndex.albumArtist.toLowerCase() === artist.toLowerCase()){
+      results.push(collectionIndex)
+    };
+  };
+  return results;
+};
 
-
-
+console.log(findByArtist(myCollection, 'In flames'));
+console.log(findByArtist(myCollection, 'ZZ Ward'));
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
